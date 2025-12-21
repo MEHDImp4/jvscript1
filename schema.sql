01814140077625f14f2678b3fba6be86adcd7645
MEHDImp4 <83414196+MEHDImp4@users.noreply.github.com>
2025-12-21 02:29:11 +0100

    fix: Correct seed user password hashes in schema.sql.

--- schema.sql
+++ schema.sql
@@ -69,8 +69,8 @@ CREATE TABLE IF NOT EXISTS Education (
 
 -- Seed data: Test users (password is MD5 of 'php123')
 INSERT INTO users (name, email, password) VALUES 
-('Chuck', 'csev@umich.edu', '1a52e17fa899cf40fb04cfc42e6352f1'),
-('UMSI', 'umsi@umich.edu', '1a52e17fa899cf40fb04cfc42e6352f1');
+('Chuck', 'csev@umich.edu', '218140990315bb39d948a523d61549b4'),
+('UMSI', 'umsi@umich.edu', '218140990315bb39d948a523d61549b4');
 
 -- Seed data: Pre-inserted institutions
 INSERT INTO Institution (name) VALUES ('University of Michigan');
